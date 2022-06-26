9b3d92b9c787aa6704a3ce899633ac27753d8fb2
Dogukan Caltekin <dcaltekin@gmail.com>
2022-06-26 15:21:35 +0300

    button-size

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,13 +13,13 @@ function Home() {
       <div className="text-3xl">Redux'tan Gelen Değer: {countValue}</div>
       <div className="my-8">
         <button
-          className="text-xl inline-block px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight  rounded shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out"
+          className="text-xl inline-block px-6 py-2.5 bg-green-500 text-white leading-tight  rounded shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out"
           onClick={() => dispatch(Arttir())}
         >
           +1 Arttır
         </button>
         <button
-          className="text-xl inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out"
+          className="text-xl inline-block px-6 py-2.5 bg-red-600 text-white leading-tight rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out"
           onClick={() => dispatch(Azalt())}
         >
           -1 Azalt
@@ -48,7 +48,7 @@ function Home() {
           onChange={(e) => setInputD(e.target.value)}
         />
         <button
-          className="text-xl inline-block px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out"
+          className="text-xl inline-block px-6 py-2.5 bg-purple-600 text-white leading-tight rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out"
           onClick={() => dispatch(InputDeger(inputD))}
         >
           +{inputD} Arttır
